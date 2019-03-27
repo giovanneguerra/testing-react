3a5b785d9af9ed0f36e38077cdfa52ea86b2c0f3
Giovanne Guerra <giovanne.guerra@fitbank.com.br>
2019-03-27 16:45:16 -0300

    Finished the tests, begun the http requests

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
-export default () => {
-    return <div>Comment Box</div>;
+class CommentBox extends Component {
+    state = { comment: '' };
+    
+    handleChange = e => {
+        this.setState({ comment: e.target.value });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        this.props.saveComment(this.state.comment);
+
+        this.setState({ comment: '' });
+    };
+
+    render (){
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h4>Add a Comment</h4>
+                <textarea onChange={this.handleChange} value={this.state.comment} />
+                <div>
+                    <button>Submit Comment</button>
+                </div>
+            </form>
+        );
+    }
 }
+
+export default connect(null, actions)(CommentBox);
