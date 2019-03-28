6c96ee4b5ad896c4854bf89efaed7da5e85d5e2c
Giovanne Guerra <giovanne.guerra@fitbank.com.br>
2019-03-28 11:38:35 -0300

    finished the tests

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -3,31 +3,36 @@ import { connect } from 'react-redux';
 import * as actions from 'actions';
 
 class CommentBox extends Component {
-    state = { comment: '' };
-    
-    handleChange = e => {
-        this.setState({ comment: e.target.value });
-    };
+	state = { comment: '' };
 
-    handleSubmit = e => {
-        e.preventDefault();
+	handleChange = (e) => {
+		this.setState({ comment: e.target.value });
+	};
 
-        this.props.saveComment(this.state.comment);
+	handleSubmit = (e) => {
+		e.preventDefault();
 
-        this.setState({ comment: '' });
-    };
+		this.props.saveComment(this.state.comment);
 
-    render (){
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <h4>Add a Comment</h4>
-                <textarea onChange={this.handleChange} value={this.state.comment} />
-                <div>
-                    <button>Submit Comment</button>
-                </div>
-            </form>
-        );
-    }
+		this.setState({ comment: '' });
+	};
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<h4>Add a Comment</h4>
+					<textarea onChange={this.handleChange} value={this.state.comment} />
+					<div>
+						<button>Submit Comment</button>
+					</div>
+				</form>
+				<button className="fetch-comments" onClick={this.props.fetchComments}>
+					Fetch Comments
+				</button>
+			</div>
+		);
+	}
 }
 
 export default connect(null, actions)(CommentBox);
